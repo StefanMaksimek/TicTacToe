82724593f0107ed41fbf7130daa50b4756ec459c
Stefan Maksimek <stefan.maksimek@gmail.com>
2022-04-27 16:45:15 +0200

    adding hoverFunction

--- script.js
+++ script.js
@@ -6,30 +6,59 @@ let LOSE_AUDIO = new Audio("#");
 
 
 function setPlayArea() {
-    for(let i = 1; i <= 9; i++) {
+    for (let i = 1; i <= 9; i++) {
         setPlayField(i);
     }
+    setNewHoverEffect("var(--main-hover)")
+    setRestartStartBtn()
 }
 
 
 function setSymbole(i) {
-    if(proofFieldEmpty(i)) {
+    if (proofFieldEmpty(i)) {
         document.getElementById(i).innerHTML = `
         <img src="./img/${activePlayer}">
     `;
-    changePlayer();
+        changePlayer(i);
     } else {
         ALERT_AUDIO.play();
     }
 }
 
 
-function changePlayer() {
-  if ( activePlayer === symbols[0]) {
-      activePlayer = symbols[1];
-  } else {
-    activePlayer = symbols[0];
-  }
+function setNewHoverEffect(b) {
+    for (let a = 1; a <= 9; a++) {
+        if (proofFieldEmpty(a)) {
+            document.getElementById(a).addEventListener("mouseover", function () {
+                document.getElementById(a).style.backgroundColor = b;
+            })
+        } else {
+            document.getElementById(a).addEventListener("mouseover", function () {
+                document.getElementById(a).style.backgroundColor = "var(--grey2-color)";
+            })
+        }
+    }
+    for (let a = 1; a <= 9; a++) {
+        if (proofFieldEmpty(a)) {
+            document.getElementById(a).addEventListener("mouseout", function () {
+                document.getElementById(a).style.backgroundColor = "var(--grey2-color)";
+            })
+        } else {
+            document.getElementById(a).style.backgroundColor = "var(--grey2-color)";
+        }
+    }
+}
+
+
+function changePlayer(i) {
+    if (activePlayer === symbols[0]) {
+        activePlayer = symbols[1];
+        setNewHoverEffect("var(--main2-hover)")
+    } else {
+        activePlayer = symbols[0];
+        setNewHoverEffect("var(--main-hover)")
+    }
+
 }
 
 
@@ -40,4 +69,9 @@ function proofFieldEmpty(i) {
 
 function setPlayField(i) {
     document.getElementById(i).innerHTML = ``;
+}
+
+
+function setRestartStartBtn() {
+    document.getElementById('start').innerHTML = `Restart`;
 }
