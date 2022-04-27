51e8f73218add3a444fdbbc094129c47d099be2f
Stefan Maksimek <stefan.maksimek@gmail.com>
2022-04-27 13:06:53 +0200

    add playFunction

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+//audio.volume = 0.5;
+let ALERT_AUDIO = new Audio("./audio/alert.mp3");
+let SUCCESS_AUDIO = new Audio("#");
+let WIN_AUDIO = new Audio("#");
+let LOSE_AUDIO = new Audio("#");
+
+
+function setPlayArea() {
+    for(let i = 1; i <= 9; i++) {
+        setPlayField(i);
+    }
+}
+
+
 function setSymbole(i) {
-    
-    document.getElementById(i).innerHTML = `
+    if(proofFieldEmpty(i)) {
+        document.getElementById(i).innerHTML = `
         <img src="./img/${activePlayer}">
     `;
     changePlayer();
+    } else {
+        ALERT_AUDIO.play();
+    }
 }
+
 
 function changePlayer() {
   if ( activePlayer === symbols[0]) {
@@ -12,4 +30,14 @@ function changePlayer() {
   } else {
     activePlayer = symbols[0];
   }
+}
+
+
+function proofFieldEmpty(i) {
+    return document.getElementById(i).innerHTML === ``;
+}
+
+
+function setPlayField(i) {
+    document.getElementById(i).innerHTML = ``;
 }
